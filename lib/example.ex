4f4b21faea0d0393773005e6a1fc33019c74e615
vinitpania <vinitpania26@gmail.com>
2024-12-12 01:24:53 +0530

    learned tupple in elixer

--- lib/example.ex
+++ lib/example.ex
@@ -6,7 +6,7 @@ defmodule Example do
 
   def start(_type, _args)do
     Example.main()
-    Example.new_year_calculator()
+    #Example.new_year_calculator()
     #IO.puts(hello())
     #IO.puts(Example.hello1())
     #IO.puts(Example.user_ID)
@@ -14,7 +14,29 @@ defmodule Example do
   end
 
   def main do
+    memberships = {:bronze, :silver}
+    IO.inspect(memberships)
+    #Atoms does not accept puts
+    memberships =  Tuple.append(memberships, :gold)
+    IO.inspect(memberships)
 
+    prices = {5,10 , 14, 16}
+    avg_prices =  Tuple.sum(prices) / tuple_size(prices)
+    IO.puts(avg_prices)
+
+    #printing out the each element in tuple
+    IO.puts("Avg of mebership from #{elem(memberships, 0)} #{elem(memberships, 1)} , #{elem(memberships, 2)} is #{avg_prices}")
+
+    #store individual instances
+    user1 = {"bint" ,  :gold}
+    {name, membership} = user1
+    IO.puts("#{elem(user1, 0)} has a #{elem(user1, 1)} membership")
+    IO.puts("#{name} has a #{membership} membership ")
+
+
+  end
+
+  def basic_date_time do
     time = Time.new(16,30,30,0)
     # IO.puts(time) --> will not wrok for multiple value
     IO.inspect(time)
@@ -27,7 +49,6 @@ defmodule Example do
     date_time = DateTime.new!(Date.new!(2024,1,1), Time.new!(0,0,0,0), "Etc/UTC")
     IO.puts(date_time.utc_offset)
     IO.inspect("Combination of Date and time is, #{date_time}")
-
   end
 
   def new_year_calculator do
@@ -47,6 +68,7 @@ defmodule Example do
     seconds = rem(time_till , 60)
     IO.puts("Seconds are #{seconds}")
 
+    IO.puts ("Days Gone: #{days} days , #{hours} hours , #{minutes} minutes, #{seconds} seconds ")
 
   end
 
